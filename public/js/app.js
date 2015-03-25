e51dbcea34f2deaf71e884df1e451504eb6b6578
santidelosrios <sdelosrios95@gmail.com>
2015-03-25 12:35:10 -0500

    Implementacion de arreglo para los vehiculos. Fabrica y controlador. Agregar funciona a medias

--- public/js/app.js
+++ public/js/app.js
@@ -1,25 +1,49 @@
-angular.module('app',['ngRoute'])
-	.config(function($routeProvider)
-    {	
-    	'use strict';
-        $routeProvider.when('/',
+var app = angular.module('app',['ngRoute']);
+app
+ 	.config(function($routeProvider)
+     {	
+     	'use strict';
+         $routeProvider.when('/',
             {
-                templateUrl: 'view/map.html'
+                controller: 'VehiculesController'
             })
-            .when('/map',
+            .otherwise(
             {
-                templateUrl: 'view/map.html'
-            })
-            .when('/vehicle',
-            {
-                templateUrl: 'view/vehicle.html'
-            })
-            .when('/remote-vehicle',
-            {
-                templateUrl: 'view/remote-vehicle.html'
-            }).otherwise(
-            {
-           		 redirectTo: '/'
-        	});
+                redirectTo: '/'
+         	})
     });
+app.factory('vehiculesFactory',function(){
+    var vehicules = [
+        {model: 'Aveo 2013',placa: 'VCH-123',state: 'on', doors: 'locked',direction: 'cra 103 #11-40',trackerNumber: '3108526996'},
+        {model: 'Aveo 2013',placa: 'VCH-123',state: 'on', doors: 'locked',direction: 'cra 103 #11-40',trackerNumber: '3108526996'},
+        {model: 'Aveo 2013',placa: 'VCH-123',state: 'on', doors: 'locked',direction: 'cra 103 #11-40',trackerNumber: '3108526996'},
+        {model: 'Aveo 2013',placa: 'VCH-123',state: 'on', doors: 'locked',direction: 'cra 103 #11-40',trackerNumber: '3108526996'},
+        {model: 'Aveo 2013',placa: 'VCH-123',state: 'on', doors: 'locked',direction: 'cra 103 #11-40',trackerNumber: '3108526996'},
+        {model: 'Aveo 2013',placa: 'VCH-123',state: 'on', doors: 'locked',direction: 'cra 103 #11-40',trackerNumber: '3108526996'},
+        {model: 'Aveo 2013',placa: 'VCH-123',state: 'on', doors: 'locked',direction: 'cra 103 #11-40',trackerNumber: '3108526996'}
+    ];
+    var factory={}; 
+    factory.getVehicules = function(){
+        return vehicules; 
+    }
+    return factory; 
+}); 
+var controllers = {};
+controllers.VehiculesController = function($scope,vehiculesFactory){
+    $scope.vehicules = vehiculesFactory.getVehicules();
+    $scope.addVehicule = function(){
+        console.log("se ejecuto");
+        $scope.vehicules.push({model: $scope.newVehicule.model,
+                               placa: $scope.newVehicule.placa,
+                               state:'off',
+                               doors: 'loocked',
+                               direction: 'nn',
+                               trackerNumber: $scope.newVehicule.trackerNumber 
+                            });
+        $scope.newVehicule.model =''; 
+        $scope.newVehicule.placa = ''; 
+        $scope.newVehicule.trackerNumber = '';
+    }
+};
+app.controller(controllers);
 	
