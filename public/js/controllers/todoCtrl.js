7a605d8641e8c1e3b86892433a4c9e471da7d866
santidelosrios <sdelosrios95@gmail.com>
2015-04-01 22:18:57 -0500

    Actualizacion funcionalidades mapa
    Mapa actualiza marker, 80% completado

--- public/js/controllers/todoCtrl.js
+++ public/js/controllers/todoCtrl.js
@@ -18,5 +18,153 @@ angular.module('app')
         $scope.actualVehicule = vehicule; 
     }  
 }]);
+    angular.module('app')
+    .controller('MapController',['$scope','vehiculesFactory', function($scope,vehiculesFactory){
+        var latLng = new google.maps.LatLng(3.364272130390176, -76.53450724334719);
+        var mapOptions = {
+         center: latLng,
+         zoom:14,
+         styles:[
+         {
+            "featureType": "landscape",
+            "elementType": "labels",
+            "stylers": [
+            {
+                "visibility": "on"
+            }
+            ]
+        },
+        {
+            "featureType": "administrative",
+            "elementType": "labels",
+            "stylers": [
+            {
+                "visibility": "on"
+            }
+            ]
+        },
+        {
+            "featureType": "administrative",
+            "elementType": "labels.text",
+            "stylers": [
+            {
+                "visibility": "on"
+            }
+            ]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "labels",
+            "stylers": [
+            {
+                "visibility": "on"
+            }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "labels",
+            "stylers": [
+            {
+                "visibility": "on"
+            }
+            ]
+        },
+        {
+            "featureType": "administrative",
+            "elementType": "labels",
+            "stylers": [
+            {
+                "visibility": "on"
+            }
+            ]
+        },
+        {
+            "featureType": "all",
+            "elementType": "labels",
+            "stylers": [
+            {
+                "visibility": "on"
+            }
+            ]
+        },
+        {
+            "featureType": "all",
+            "elementType": "labels",
+            "stylers": [
+            {
+                "visibility": "on"
+            }
+            ]
+        }
+        ]                       
+    }
+    var checkbox = true; 
+    var map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+    var arrMarkers = []; 
+    var incializeMarkers = function(){ 
+        var locations = vehiculesFactory.getLocations();
+        var markers = [];
+        for (var i=0;i<locations.length;i++){
+            arrMarkers.push( new google.maps.Marker({
+                        position: new google.maps.LatLng(locations[i].lat,locations[i].lng),
+                        map: map,
+                        visible: true,
+                        icon:'http://gavinwillis.co.uk/newimages/pointer.png'
+                    }));
+        }  
+    }
+    incializeMarkers();
+
+    var showTheMarker = function(id){
+        var pointers = document.getElementById(id);
+        pointers.className = 'map-pointer';
+        arrMarkers[id].setVisible(true);
+
+    }
+    var hideTheMarker = function(id){       
+        var pointers = document.getElementById(id);
+        pointers.className = 'unpointer';
+        arrMarkers[id].setVisible(false); 
+    }
+    var showAllMarkers = function(){
+        for( var i =0 ; i < arrMarkers.length; i++ ) {
+            //$scope.arrMarkers[i].setMap($scope.map);
+            arrMarkers[i].setVisible(true);
+            var pointers = document.getElementById(i);
+            pointers.className = 'map-pointer'
+        }
+        checkbox=true;
+
+    }
+    var hideAllMarkers = function(){
+        for( var i = 0; i < arrMarkers.length; i++ ) {
+            //$scope.arrMarkers[0].setMap(null);
+            arrMarkers[i].setVisible(false); 
+            var pointers = document.getElementById(i);
+            pointers.className = 'unpointer'
+        }  
+        checkbox = false;
+    }
+    $scope.selectAll = function(){
+        if(checkbox){
+            hideAllMarkers();
+        }else{
+            showAllMarkers(); 
+        }
+    } 
+    $scope.selectTheMarker = function(id){ 
+        var theMarker = arrMarkers[0]
+        console.log(theMarker)
+        if(arrMarkers[id].getVisible()){
+            hideTheMarker(id);
+        }else{
+            showTheMarker(id);
+            //comentario
+        }
+    } 
+
+}]);
+
 
 	
--- public/js/factory/vehicleFactory.js
+++ public/js/factory/vehicleFactory.js
@@ -1,17 +1,26 @@
 angular.module('app')
     .factory('vehiculesFactory',function(){
     var vehicules = [
-        {id: 1,model: 'Aveo 2013',placa: 'VCH-123',state: 'prendido', doors: 'cerradas',address: 'cra 103 #11-40',trackerNumber: '3108526996'},
-        {id: 2,model: 'Captiva 2013',placa: 'VCH-123',state: 'apagado', doors: 'abiertas',address: 'cra 103 #11-40',trackerNumber: '3108526996'},
-        {id: 3,model: 'Tucson 2013',placa: 'VCH-123',state: 'prendido', doors: 'cerradas',address: 'cra 103 #11-40',trackerNumber: '3108526996'},
-        {id: 4,model: 'Tiida 2013',placa: 'VCH-123',state: 'apagado', doors: 'abiertas',address: 'cra 103 #11-40',trackerNumber: '3108526996'},
-        {id: 5,model: 'Picanto ion 2013',placa: 'VCH-123',state: 'prendido', doors: 'cerradas',address: 'cra 103 #11-40',trackerNumber: '3108526996'},
-        {id: 6,model: 'Cruze 2013',placa: 'VCH-123',state: 'apagado', doors: 'abiertas',address: 'cra 103 #11-40',trackerNumber: '3108526996'},
-        {id: 7,model: 'Mazda 3 2013',placa: 'VCH-123',state: 'prendido', doors: 'cerradas',address: 'cra 103 #11-40',trackerNumber: '3108526996'}
+        {id: 1,model: 'Aveo 2013',placa: 'VCH-123',state: 'prendido', doors: 'cerradas',address: 'cra 103 #11-40',trackerNumber: '3108526996',lat:'3.3653110334126453',lng:'-76.54007550926211',visible: true},
+        {id: 2,model: 'Captiva 2013',placa: 'VCH-123',state: 'apagado', doors: 'abiertas',address: 'cra 103 #11-40',trackerNumber: '3108526996',lat:'3.36531103341262',lng:'-76.53755423278811',visible: true},
+        {id: 3,model: 'Tucson 2013',placa: 'VCH-123',state: 'prendido', doors: 'cerradas',address: 'cra 103 #11-40',trackerNumber: '3108526996',lat:'3.367431677381319',lng:'-76.53700706214907',visible: true},
+        {id: 4,model: 'Tiida 2013',placa: 'VCH-123',state: 'apagado', doors: 'abiertas',address: 'cra 103 #11-40',trackerNumber: '3108526996',lat:'3.364272130390176',lng:'-76.53450724334719',visible: true},
+        {id: 5,model: 'Picanto ion 2013',placa: 'VCH-123',state: 'prendido', doors: 'cerradas',address: 'cra 103 #11-40',trackerNumber: '3108526996',lat:'3.3623228454421734',lng:'-76.53152462692263',visible: true},
+        {id: 6,model: 'Cruze 2013',placa: 'VCH-123',state: 'apagado', doors: 'abiertas',address: 'cra 103 #11-40',trackerNumber: '3108526996',lat:'3.3585420783667863',lng:'-76.53024789543154',visible: true},
+        {id: 7,model: 'Mazda 3 2013',placa: 'VCH-123',state: 'prendido', doors: 'cerradas',address: 'cra 103 #11-40',trackerNumber: '3108526996',lat:'3.352929807897045',lng:'-76.53046247215273',visible: true}
     ];
     var factory={}; 
     factory.getVehicules = function(){
         return vehicules; 
+    }
+    //comentario
+    factory.getLocations = function(){
+        var locations = []; 
+        for (var i = 0; i < vehicules.length; i++) {
+            var theVehicule = vehicules[i];
+            locations[i] = {lat: theVehicule.lat, lng: theVehicule.lng, visible: theVehicule.visible};
+        }
+        return locations; 
     }
     return factory; 
 }); 
